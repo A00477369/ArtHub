25144dc5759e76c2fbe94719fe575943da7dc112
Pranay Malusare <pranaymalu534@gmail.com>
2023-11-30 01:46:26 -0400

    Category service file partially compeleted

--- Services/ServicesImpl/CategoryServiceImpl.cs
+++ Services/ServicesImpl/CategoryServiceImpl.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using ArtHub.dto;
 using ArtHub.Models;
 
@@ -7,13 +9,17 @@ namespace ArtHub.Services.ServicesImpl
     public class CategoryServiceImpl : CategoryService
     {
         private readonly AppDbContext _context;
+
         public CategoryServiceImpl(AppDbContext context)
         {
             _context = context;
         }
+
         public Category CreateCategory(Category category)
         {
-            throw new NotImplementedException();
+            _context.Category.Add(category);
+            _context.SaveChanges();
+            return category;
         }
 
         public List<Category> GetAllCategories()
@@ -24,13 +30,20 @@ namespace ArtHub.Services.ServicesImpl
 
         public Category GetCategoryById(int id)
         {
-            return _context.Category.Find(id);
+            Category category = _context.Category.Find(id);
+            return category;
         }
 
         public Category UpdateCategory(UpdateCategoryDto dto, Category existingCategory)
         {
-            throw new NotImplementedException();
+            if (existingCategory != null)
+            {
+                existingCategory.Title = dto.Title;
+         
+                _context.SaveChanges();
+            }
+
+            return existingCategory;
         }
     }
 }
-
